681df0391d5abd896244189f9f745f985bec821d
Krina Wibisana <krinawibisana@gmail.com>
2020-05-15 23:58:58 +0700

    master: add new files for eFishery Test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from "react";
-import PostForm from "./containers/PostForm";
-import AllPost from "./containers/AllPost";
-import { Container } from 'reactstrap';
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Container>
-          <h2>efishery test</h2>
-          <br />
-        </Container>
-        <PostForm />
-        <AllPost />
-      </Container>
-    );
-  }
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
