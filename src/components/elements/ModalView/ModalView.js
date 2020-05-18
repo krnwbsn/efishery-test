08ca6e0af66de132138411efa20b967e687cc161
Krina Wibisana <krinawibisana@gmail.com>
2020-05-18 13:51:36 +0700

    Edit: remove edit feature, work in progress

--- src/components/elements/ModalView/ModalView.js
+++ src/components/elements/ModalView/ModalView.js
@@ -26,6 +26,17 @@ const useStyles = makeStyles((theme) => ({
     border: '3px solid rgb(91, 181, 154)',
     padding: theme.spacing(2, 4, 3),
   },
+  editButton: {
+    '&:hover': {
+      backgroundColor: 'rgba(24, 51, 88, 0.9)'
+    },
+    margin: theme.spacing(1),
+    marginRight: 0,
+    marginLeft: 0,
+    backgroundColor: 'rgb(24, 51, 88)',
+    textTransform: 'none',
+    minWidth: 75
+  },
 }));
 
 export default function ModalView(props) {
@@ -48,7 +59,7 @@ export default function ModalView(props) {
         onClick={handleOpen}
       >
         Tambah Data
-      </Button>
+        </Button>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,11 +1,9 @@
 import FormAdd from './FormAdd';
-import FormEdit from './FormEdit';
 import SelectArea from './SelectArea';
 import SelectSize from './SelectSize';
 
 export {
   FormAdd,
-  FormEdit,
   SelectArea,
   SelectSize
 };
